ef9e9b4387e2a9cd4851a4f3aaab28b17876d926
Pawan <pawanb78@gmail.com>
2023-12-30 19:40:04 +0000

    server created

---
+++ RealState/client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/Profile' element={<Profile/>}/>
+        <Route path='/SignIn' element={<SignIn/>}/>
+        <Route path='/SignUp' element={<SignUp/>}/>
+      </Routes>
+      </BrowserRouter>
+  )
+}
+ 
