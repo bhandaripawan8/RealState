0c8d11283af472b0dde8f89e4f7c780748283cc2
Pawan <pawanb78@gmail.com>
2023-12-31 13:23:37 +0000

    created a middleware and function to handle possible error

--- RealState/api/controller/auth.controller.js
+++ RealState/api/controller/auth.controller.js
@@ -1,16 +1,14 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-export const signup = async(req, res) => {
+
+export const signup = async(req, res, next) => {
      const {username, email, password} = req.body;
      const hashedPassword = bcryptjs.hashSync(password, 10);
      try {
         await newUser.save();
         res.status(201).json('user created successfully');
      } catch(error){
-        res.status(500).json(error.message);
+       next(error);
      }
-       
-
-     const newUser = new User({username, email, password: hashedPassword});
-   
+     const newUser = new User({username, email, password: hashedPassword});  
 }
