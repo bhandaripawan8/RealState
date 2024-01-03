d671717aea7f2095e753a5c4b4dcff22593e473f
Pawan <pawanb78@gmail.com>
2024-01-03 00:30:23 +0000

    sign in api route completed

--- RealState/api/routes/auth.route.js
+++ RealState/api/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import {signup} from '../controller/auth.controller.js';
+import {signIn} from '../controller/auth.controller.js';
 
 const router = express();
 
 router.post("/signup", signup)
+router.post("/signIn", signIn)
 
 export default router;
